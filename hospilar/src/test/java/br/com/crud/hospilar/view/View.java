e0e325dd17c8d754d1a34bafce32bb9b4d567574
tassioplima <tassiopelima@gmail.com>
2017-08-26 01:43:10 -0300

    Atualização View

--- hospilar/src/test/java/br/com/crud/hospilar/view/View.java
+++ hospilar/src/test/java/br/com/crud/hospilar/view/View.java
@@ -12,11 +12,22 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import java.awt.Toolkit;
+import javax.swing.JScrollPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JRadioButton;
 
 public class View extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel getContentPane;
 	private JTextField txtNome;
+	private JTextField txtIdade;
+	private JTextField txtSangue;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,12 +49,18 @@ public class View extends JFrame {
 	 * Create the frame.
 	 */
 	public View() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\tassi\\git\\HospiLar\\hospilar\\hospital.png"));
+		setTitle("Hospilar");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		setBounds(100, 100, 567, 360);
+		getContentPane = new JPanel();
+		getContentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(getContentPane);
+		getContentPane.setLayout(null);
+		
+		JPanel painelBotoes = new JPanel();
+		painelBotoes.setBounds(0, 0, 551, 90);
+		getContentPane.add(painelBotoes);
 		
 		JButton btnRegistrar = new JButton("Registrar");
 		btnRegistrar.addActionListener(new ActionListener() {
@@ -53,36 +70,123 @@ public class View extends JFrame {
 				
 			}
 		});
-		btnRegistrar.setBounds(14, 212, 91, 38);
-		contentPane.add(btnRegistrar);
-		
-		txtNome = new JTextField();
-		txtNome.setBounds(53, 29, 325, 20);
-		contentPane.add(txtNome);
-		txtNome.setColumns(10);
-		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(55, 11, 46, 14);
-		contentPane.add(lblNome);
 		
 		JButton btnConsultar = new JButton("Consultar");
 		btnConsultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnConsultar.setBounds(119, 212, 91, 38);
-		contentPane.add(btnConsultar);
 		
 		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setBounds(224, 212, 91, 38);
-		contentPane.add(btnAlterar);
 		
 		JButton btnDeletar = new JButton("Deletar");
 		btnDeletar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnDeletar.setBounds(329, 212, 91, 38);
-		contentPane.add(btnDeletar);
+		GroupLayout gl_painelBotoes = new GroupLayout(painelBotoes);
+		gl_painelBotoes.setHorizontalGroup(
+			gl_painelBotoes.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelBotoes.createSequentialGroup()
+					.addGap(38)
+					.addComponent(btnRegistrar, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(btnConsultar, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(btnAlterar, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(btnDeletar, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(38, Short.MAX_VALUE))
+		);
+		gl_painelBotoes.setVerticalGroup(
+			gl_painelBotoes.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelBotoes.createSequentialGroup()
+					.addGap(26)
+					.addGroup(gl_painelBotoes.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnRegistrar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnConsultar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAlterar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnDeletar, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(26, Short.MAX_VALUE))
+		);
+		gl_painelBotoes.linkSize(SwingConstants.VERTICAL, new Component[] {btnRegistrar, btnConsultar, btnAlterar, btnDeletar});
+		gl_painelBotoes.linkSize(SwingConstants.HORIZONTAL, new Component[] {btnRegistrar, btnConsultar, btnAlterar, btnDeletar});
+		painelBotoes.setLayout(gl_painelBotoes);
+		
+		JPanel painelRegistros = new JPanel();
+		painelRegistros.setBounds(0, 89, 551, 102);
+		getContentPane.add(painelRegistros);
+		
+		txtNome = new JTextField();
+		txtNome.setColumns(10);
+		
+		JLabel lblNome = new JLabel("Nome Paciente");
+		
+		JLabel lblNewLabel = new JLabel("Idade");
+		
+		txtIdade = new JTextField();
+		txtIdade.setColumns(10);
+		
+		JRadioButton rdbtnMasculino = new JRadioButton("Masculino");
+		
+		JRadioButton rdbtnFeminino = new JRadioButton("Feminino");
+		
+		txtSangue = new JTextField();
+		txtSangue.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Tipo Sanguineo");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		GroupLayout gl_painelRegistros = new GroupLayout(painelRegistros);
+		gl_painelRegistros.setHorizontalGroup(
+			gl_painelRegistros.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelRegistros.createSequentialGroup()
+					.addGap(3)
+					.addGroup(gl_painelRegistros.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNome)
+						.addComponent(lblNewLabel))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_painelRegistros.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_painelRegistros.createSequentialGroup()
+							.addGap(10)
+							.addComponent(textField))
+						.addGroup(gl_painelRegistros.createSequentialGroup()
+							.addComponent(txtIdade, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(lblNewLabel_1)
+							.addGap(18)
+							.addComponent(txtSangue, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, 247, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(rdbtnMasculino)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(rdbtnFeminino)
+					.addContainerGap(28, Short.MAX_VALUE))
+		);
+		gl_painelRegistros.setVerticalGroup(
+			gl_painelRegistros.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_painelRegistros.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_painelRegistros.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNome)
+						.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(rdbtnMasculino)
+						.addComponent(rdbtnFeminino))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_painelRegistros.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(txtIdade, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtSangue, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(19, Short.MAX_VALUE))
+		);
+		painelRegistros.setLayout(gl_painelRegistros);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 192, 551, 118);
+		getContentPane.add(scrollPane);
 	}
 }
