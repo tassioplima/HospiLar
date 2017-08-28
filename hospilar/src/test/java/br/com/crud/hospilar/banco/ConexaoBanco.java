d5eb9c530e16618d573a28d817c7db58867c245e
tassioplima <tassiopelima@gmail.com>
2017-08-28 00:33:34 -0300

    COnexao banco

--- hospilar/src/test/java/br/com/crud/hospilar/banco/ConexaoBanco.java
+++ hospilar/src/test/java/br/com/crud/hospilar/banco/ConexaoBanco.java
@@ -2,24 +2,79 @@ package br.com.crud.hospilar.banco;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import com.mysql.jdbc.PreparedStatement;
+
+
 
 public class  ConexaoBanco {
 	
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql:localhost:3306/cadastropaciente";
+	private static final String USER = "root";
+	private static final String PASS = "root";
 	 
-	 Connection conexao = null;
-	 
-	 public static void conexaoBanco()  throws SQLException{
-		 
-		 try{
-			 		Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastropaciente", "root", "root");
-			 		System.out.println("Conectado com o Bando de Dados");
-			 		conexao.close();
-			 		} 	catch(SQLException e)  {
-			 			
-			 			
-			 	}
+	 public static Connection abrirConexao() throws SQLException{
 		
-	}
+		 try {
+			Class.forName(DRIVER);
+			
+			return DriverManager.getConnection(URL, USER, PASS);
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException("Erro na conexao: ", e);
+		} 
+		
+		
+	 }
+	 
+	 public static void fecharConexao(Connection con){
+		 
+		 if (con!=null){
+			 try {
+				 con.close();
+			 } catch (SQLException e) {
+				 
+			 }
+			 
+		 }
+	 }
+	 
+public static void fecharConexao(Connection con, PreparedStatement stmt){
+	
+		fecharConexao(con);
+	
+		 try {
+			 
+			 if(stmt!=null);
+			 
+			 stmt.close();
+		 
+		 }catch (SQLException e) {
+				 
+			 }
+			 
+		 }
 
-}
+public static void fecharConexao(Connection con, PreparedStatement stmt, ResultSet rs){
+	
+	fecharConexao(con, stmt);
+
+	 try {
+		 
+		 if(rs!=null);
+		 
+		 rs.close();
+	 
+	 }catch (SQLException e) {
+			 
+		 }
+		 
+	 }
+	 } 
+	 
+	
+
--- hospilar/src/test/java/br/com/crud/hospilar/entidades/PacienteDAO.java
+++ hospilar/src/test/java/br/com/crud/hospilar/entidades/PacienteDAO.java
@@ -1,22 +1,43 @@
 package br.com.crud.hospilar.entidades;
 
-import java.awt.List;
 import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
+import br.com.crud.hospilar.banco.ConexaoBanco;
 
 public class PacienteDAO {
 
-	private int id;
-	private String name;
-	private  String email;
-	
-	public List<Paciente> listarDado(){
+	public void salvar(Paciente p) throws SQLException {
+
+		Connection con = ConexaoBanco.abrirConexao();
+		java.sql.PreparedStatement stmt = null;
+
+		try {
+			stmt = con.prepareStatement(
+					"INSERT INTO cadastropaciente (nome, idade, tsanguineo, sexo, cpf, endereco, email, sintomas) VALUES(?,?,?,?,?,?,?,?,?) ");
+			stmt.setString(1, p.getNome() );
+			stmt.setInt(2, p.getIdade());
+			stmt.setString(1, p.getTsanguineo());
+			stmt.setString(1, p.getSexo());
+			stmt.setInt(1, p.getCpf() );
+			stmt.setString(1, p.getEndereco() );
+			stmt.setString(1, p.getEmail() );
+			stmt.setString(1, p.getSintomas());
+			
+			stmt.executeUpdate();
+			
 		
-		
-		
-		
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}finally{
+			
+			ConexaoBanco.fecharConexao(con);
+		}
+
 	}
-	
-	
-	
+
 }
